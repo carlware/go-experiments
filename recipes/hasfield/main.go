6acc06303a1134daac58b06033b3818104ed241c
Carlos Ramirez <carlware.sys@gmail.com>
2020-06-10 23:32:04 -0500

    testing embedded structs

--- recipes/hasfield/main.go
+++ recipes/hasfield/main.go
@@ -10,4 +10,18 @@ func main() {
 	fmt.Println("has name", src.HasField(arg1, "name"))
 	fmt.Println("has age", src.HasField(arg1, "age"))
 
+	arg2 := &struct {
+		Random int
+		Owner  *struct {
+			Address struct {
+				Name string
+				Cp   int
+			}
+		}
+	}{}
+
+	fmt.Println("has random", src.HasField(arg2, "random"))
+	fmt.Println("has owner.address", src.HasField(arg2, "owner.address"))
+	fmt.Println("has owner.address.age", src.HasField(arg2, "owner.address.age"))
+	fmt.Println("has owner.address.cp", src.HasField(arg2, "owner.address.cp"))
 }
