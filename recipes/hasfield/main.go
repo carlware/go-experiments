cf98d7f02da016959d609a49c6adf13901141b00
Carlos Ramirez <carlware.sys@gmail.com>
2020-06-10 23:28:27 -0500

    testing the method

--- recipes/hasfield/main.go
+++ recipes/hasfield/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"carlware/hasfield/src"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello")
+	arg1 := struct{ Name string }{}
+	fmt.Println("has name", src.HasField(arg1, "name"))
+	fmt.Println("has age", src.HasField(arg1, "age"))
+
 }
--- recipes/hasfield/src/has.go
+++ recipes/hasfield/src/has.go
@@ -1,1 +1,36 @@
 package src
+
+import (
+	"reflect"
+	"strings"
+)
+
+func getValue(st interface{}) reflect.Value {
+	vIface := reflect.ValueOf(st)
+
+	// Check if the passed interface is a pointer
+	if vIface.Type().Kind() != reflect.Ptr {
+		vIface = reflect.New(reflect.TypeOf(st))
+	}
+	return vIface
+}
+
+func HasField(st interface{}, ex string) bool {
+	i := st
+	elem := getValue(i).Elem()
+	for _, token := range strings.Split(ex, ".") {
+		field := elem.FieldByName(strings.Title(token))
+		if !field.IsValid() {
+			return false
+		}
+		kind := field.Type()
+		if kind.Kind() == reflect.Ptr {
+			// dereferencing a nil pointer will always return reflect.Invalid. The solution is to create a new instance first
+			elem = reflect.New(field.Type().Elem()).Elem()
+		}
+		if kind.Kind() == reflect.Struct {
+			elem = getValue(field.Interface()).Elem()
+		}
+	}
+	return true
+}
