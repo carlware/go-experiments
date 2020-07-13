3b97176733fdb66b088aa46f3b038b3748d2fdf8
Carlos Ramirez <carlware.sys@gmail.com>
2020-07-13 12:13:10 -0500

    added upload image

--- gcs/main.go
+++ gcs/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
-	"strings"
+	"os"
 
 	"github.com/viant/afs"
 	"github.com/viant/afs/option"
@@ -16,6 +16,7 @@ import (
 
 const folderName = "gs://proofs-staging/proofs"
 const secretPath = "key.json"
+const filePath = "cats.jpeg"
 
 func main() {
 	jwtConfig, err := gs.NewJwtConfig(option.NewLocation(secretPath))
@@ -29,22 +30,43 @@ func main() {
 	jsonAuth := goption.WithCredentialsJSON(JSON)
 	opt1 := gs.NewClientOptions(jsonAuth)
 
-	service := afs.New()
-	ctx := context.Background()
-	objects, err := service.List(ctx, folderName, opt1)
+	buf, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, object := range objects {
-		url := object.URL()
-		fmt.Printf("%v %v\n", object.Name(), strings.Replace(url, "gs://", "https://storage.googleapis.com/", 1))
-		if object.IsDir() {
-			continue
-		}
-	}
+	r := bufio.NewReader(buf)
 
-	err = service.Copy(ctx, folderName, "tmp")
+	ctx := context.Background()
+	service := afs.New()
+	err = service.Upload(ctx, folderName+"/cats.jpeg", 0644, r, opt1)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// reader, err := service.DownloadWithURL(ctx, folderName+"/cats.jpeg", opt1)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// data, err := ioutil.ReadAll(reader)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("data: %s\n", data)
+
+	// service := afs.New()
+	// ctx := context.Background()
+	// objects, err := service.List(ctx, folderName, opt1)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for _, object := range objects {
+	// 	url := object.URL()
+	// 	fmt.Printf("%v %v\n", object.Name(), strings.Replace(url, "gs://", "https://storage.googleapis.com/", 1))
+	// 	if object.IsDir() {
+	// 		continue
+	// 	}
+	// }
+	// err = service.Copy(ctx, folderName, "tmp")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
